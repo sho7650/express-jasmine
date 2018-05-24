7adfb1957eb1ff27a6c2a8e09f149816ebce921f
sho kisaragi <sho@oshiire.to>
2018-05-24 21:47:55 +0900

    using supertest

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,12 +1,17 @@
+var app = require('../app.js');
+
 var request = require('supertest');
 var base_url = 'http://localhost:3000/';
 
 describe("/", function () {
   describe("GET /", function () {
     it("returns OK, ", function () {
-      request(base_url)
+      request(app)
       .get('/')
-      .expect(200);
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+      });      
     })
   })
 });
